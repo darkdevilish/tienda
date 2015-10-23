eeb6b3ff7a2a10bb70236ebe5c70615ef209597b
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-10-23 02:31:13 -0400

    Adds store controller and routes to index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+  root 'store#index'
+  get 'store/:id' => 'store#show', as: :store_product
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
